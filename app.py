29b84950f9d2d7e29246b5854b4cb12c6c8e7b3f
Marcus Ng <marcuslistanng@gmail.com>
2017-11-19 21:23:36 -0500

    Find events

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-import os
-from flask import Flask, redirect, url_for, render_template, session, \
-	request, flash
+from utils.maps import *
+from utils.events import *
+
+from flask import Flask, redirect, url_for, render_template, session, request, flash
+import requests
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def root():
+    return redirect(url_for('map'))
+    
 @app.route("/map", methods=["GET", "POST"])
 def map():
+    return render_template('map.html')
 
 @app.route("/event", methods=["GET"])
 def event():
-
+    print(find_events(40.713, -74.007, 1))
+    return render_template('event.html')
 
 if __name__ == "__main__":
 	app.debug = True
--- utils/events.py
+++ utils/events.py
@@ -1,1 +1,22 @@
-.
+import requests
+
+api_key = 'a234059652f51166754647e41401e7'
+
+# Find events - Returns array of nearby events containing information
+def find_events(lat, long, radius):
+    events = []
+    url = 'https://api.meetup.com/2/open_events?&sign=true&photo-host=public&lat=' + str(lat) + '&lon=' + str(long) + '&radius=' + str(radius) + '&page=20&key=' + api_key
+    r = requests.get(url)
+    data = r.json()
+    for event in data['results']:
+        if event.get('venue'):
+            new_event = []
+            new_event.append(event['name'])
+            new_event.append(event['time'])
+            new_event.append(event['description'])
+            new_event.append(event['venue']['lat'])
+            new_event.append(event['venue']['lon'])
+            events.append(new_event)
+        
+    return events
+
