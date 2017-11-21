964541c7047cb8548f919f7341cf9cd7440fd9eb
Marcus Ng <marcuslistanng@gmail.com>
2017-11-20 23:50:52 -0500

    Event search works

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from utils.maps import *
 from utils.events import *
 
 from flask import Flask, redirect, url_for, render_template, session, request, flash
-import requests
+import requests, os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(64)
 
 @app.route("/", methods=["GET"])
 def root():
@@ -12,7 +13,20 @@ def root():
     
 @app.route("/map", methods=["GET", "POST"])
 def map():
-    events = find_events(40.713, -74.007, 1)
+    events = []
+
+    if request.method == 'POST':
+        try:
+            address = request.form["address"]
+            radius = request.form["radius"]
+
+            # Convert address to coordinates
+            coords = convert_address(address)
+            events = find_events(coords[0], coords[1], radius)
+            return render_template('map.html', events=events, location=coords)
+        except:
+            flash("Please enter an address and radius.")
+            
     return render_template('map.html', events=events)
 
 @app.route("/event", methods=["GET"])
--- utils/maps.py
+++ utils/maps.py
@@ -1,4 +1,16 @@
 import requests
 
+# Get latitude and longitude of address - Returns array [lat,lon]
+def convert_address(address):
+    query = address.replace(' ', '%20')
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?&address=' + query
+    r = requests.get(url)
+    data = r.json()
+    if data['status'] == 'OK':
+        location = data['results'][0]['geometry']['location']
+        lat = location['lat']
+        lon = location['lng']
+        return [lat,lon]
+
 
 
