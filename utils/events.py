e492b19477c91469c05c25bb4e93351510e196ae
Marcus Ng <marcuslistanng@gmail.com>
2017-11-20 21:37:18 -0500

    new design doc site mockups

--- utils/events.py
+++ utils/events.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = 'API_KEY'
+api_key = 'a234059652f51166754647e41401e7'
 
 # Find events - Returns array of nearby events containing information
 def find_events(lat, long, radius):
